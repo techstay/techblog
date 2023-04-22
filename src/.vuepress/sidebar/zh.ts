b7054dee637738743e130c923d85d1f5807aceca
techstay <lovery521@gmail.com>
2023-04-22 18:22:48 +0800

    :memo: 添加了技能的文章

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -4,6 +4,12 @@ export const zhSidebar = sidebar({
   "/": [
     "",
     {
+      text: "技能",
+      icon: "speed",
+      prefix: "skill/",
+      children: "structure"
+    },
+    {
       text: "文章",
       icon: "blog",
       prefix: "posts/",
