26327308f65972dcfab554b6a3fdc67f51ca77b6
techstay <lovery521@gmail.com>
2023-04-22 18:22:48 +0800

    :memo: 添加了技能的文章

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -2,5 +2,6 @@ import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
   "/",
+  { 'text': '技能', icon: "speed", link: "/skill/" },
   { text: "文章", icon: "blog", link: "/posts/" },
 ]);
